571c51c5991a8f01f540aaa89a25fe6e314dd2ea
Abhishek mirzapur <92161253+Kalinbhaiya143@users.noreply.github.com>
2022-05-25 21:48:15 +0530

    Area

---
+++ Area.py
@@ -0,0 +1,13 @@
+from sys import float_info
+
+
+s=float(input("Enter the side of squre is:"))
+a=s*s
+print(f"Area of squre is{a}")
+b=float(input("Enter the radius of circle is:"))
+c=3.14*b*b
+print(f"Area of circle is{c}")
+d=float(input("Enter the length is:"))
+e=float(input("Enter the breath is:"))
+f=d*e
+print(f"Area of rectangle is{f}")

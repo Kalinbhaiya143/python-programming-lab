97ed80e072bf44121c4501bb46884bf6fdfc9ef2
Abhishek mirzapur <92161253+Kalinbhaiya143@users.noreply.github.com>
2022-05-29 12:42:10 +0530

    numpy floor

---
+++ numpyfloor.py
@@ -0,0 +1,13 @@
+
+# floor_divide() function
+import numpy as np
+
+# input_array
+arr1 = [2, 6, 21, 21, 12]
+arr2 = [2, 3, 4, -3, 6]
+print ("arr1		 : ", arr1)
+print ("arr2		 : ", arr2)
+
+# output_array
+out = np.floor_divide(arr1, arr2)
+print ("\nOutput array : ", out)

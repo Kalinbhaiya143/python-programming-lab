8dbf00fa6db2d2561efe7907541c6a555d35a9d5
Abhishek mirzapur <92161253+Kalinbhaiya143@users.noreply.github.com>
2022-05-29 11:56:23 +0530

    2nd way length

---
+++ listlen2.py
@@ -0,0 +1,17 @@
+# Python code to demonstrate
+# length of list
+test_list = [ 1, 4, 5, 7, 8 ]
+
+# Printing test_list
+print ("The list is : " + str(test_list))
+
+# Finding length of list
+# using loop
+counter = 0
+for i in test_list:
+	
+	
+	counter = counter + 1
+
+# Printing length of list
+print ("Length of list using naive method is : " + str(counter))

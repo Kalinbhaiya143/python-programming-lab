c2e2c26c7355faca034e3a7cb3d8585008c640bb
Abhishek mirzapur <92161253+Kalinbhaiya143@users.noreply.github.com>
2022-05-29 12:28:24 +0530

    floor divide

---
+++ 2ndwayfloor.py
@@ -0,0 +1,12 @@
+# Python program explaining
+# floor_divide() function
+import numpy as np
+
+# input_array
+arr1 = [2, 7, 3, 11, 4]
+divisor = 3
+print ("arr1		 : ", arr1)
+
+# output_array
+out = np.floor_divide(arr1, divisor)
+print ("\nOutput array : ", out)
